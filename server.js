e1e3df9e6a807759cd2227d286f676d5e3c9ed4d
Robin La Rondelle <ra.larondelle@student.avans.nl>
2018-12-15 05:49:15 +0100

    Changed Server and DB connection

--- server.js
+++ server.js
@@ -1,45 +1,50 @@
-if (process.env.NODE_ENV == "test") {  
-  require("dotenv").config({path: "./src/config/dev.enviroment.env"})  
-}
+if (process.env.NODE_ENV === "test" || "test-cloud") require("dotenv").config({ path: "./src/config/enviroment.env" })
+if (process.env.NODE_ENV === "local" || "cloud") require("dotenv").config({ path: "./src/config/enviroment.env" })
 
 const express = require('express')
-const morgan = require("morgan");
+const morgan = require("morgan")
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require("cors")
 const passport = require("passport")
 
 const ApiError = require("./src/models/apierror.model")
-const cfg = require("./src/config/config.json")
 require("./src/config/passport")
 
 const userRoutes = require("./src/routes/user.routes")
 const applicationRoutes = require("./src/routes/application.routes")
 const postRoutes = require("./src/routes/post.routes")
 const authenticationRoutes = require("./src/routes/authentication.routes")
-const authenticationController = require("./src/controllers/authentication.controller")
 
-const port = process.env.PORT || cfg.PORT
+const port = process.env.PORT || "3000"
 const app = express()
 
-
-if(process.env.NODE_ENV !== "test") {
-  mongoose.connect(process.env.DB_URL, {
+if (process.env.NODE_ENV === "local") {
+  mongoose.connect(process.env.DB_LOCAL_URL, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true
   })
 
   mongoose.connection
-  .once('open', () => console.log("Opened cloud DB\n"))
-  .on('error', (error) => console.warn("Error", error))
+    .once('open', () => console.log("Opened local DB\n"))
+    .on('error', (error) => console.warn("Error", error))
+
+} else if (process.env.NODE_ENV === "cloud") {
+  mongoose.connect(process.env.DB_CLOUD_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+
+  mongoose.connection
+    .once('open', () => console.log("Opened cloud DB\n"))
+    .on('error', (error) => console.warn("Error", error))
 }
 
 app.use(bodyParser.json())
 app.use(morgan("dev"))
-
-//response is coming from angular
-app.use(cors({origin: `https://robinlarondelle.github.io`}))
+app.use(cors({ origin: "*" }))
 app.use(passport.initialize())
 
 app.use("/api", authenticationRoutes)
@@ -47,7 +52,7 @@ app.use("/api/users", userRoutes)
 app.use("/api/applications", applicationRoutes)
 app.use("/api/posts", postRoutes)
 
-app.use("*", function(req, res, next) {
+app.use("*", function (req, res, next) {
   next(new ApiError("Deze endpoint bestaat niet", 404, Date.now()))
 })
 
@@ -55,7 +60,7 @@ app.use((err, req, res, next) => {
   res.status(err.code || 404).json(err).send();
 })
 
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== "test" || "test-cloud") {
   app.listen(port, () => {
     console.log("Server is running on port: " + port)
   })
--- test/setup.test.js
+++ test/setup.test.js
@@ -3,15 +3,28 @@ const cfg = require("../src/config/config.json")
 
 
 before("Connect to MongoDB", done => {
-  mongoose.connect(process.env.DB_TEST_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
-
-  mongoose.connection
-    .once("open", () => { console.log("Opened Test DB \n"); done() })
-    .on("error", ((error) => { console.warn("Error: " + error); done(); }))
+  if (process.env.NODE_ENV === "test-local") {
+    mongoose.connect(process.env.DB_LOCAL_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+  
+    mongoose.connection
+    .once('open', () => console.log("Opened local DB\n"))
+    .on('error', (error) => console.warn("Error", error))
+    
+  } else if (process.env.NODE_ENV === "test-cloud") {
+    mongoose.connect(process.env.DB_CLOUD_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+  
+    mongoose.connection
+    .once('open', () => console.log("Opened cloud DB\n"))
+    .on('error', (error) => console.warn("Error", error))
+  }
 })
 
 beforeEach("Clear database", done => {
